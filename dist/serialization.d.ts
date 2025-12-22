127c90eba868a40cfe7eb24621287d8c65414c68
Oriun <emmanuel@nuiro.me>
2025-12-22 06:50:37 +0100

    Delete built distribution files and maps from dist/ directory and update package.json scripts for packaging and testing

--- dist/serialization.d.ts
+++
@@ -1,6 +0,0 @@
-import type { PgIndexDefinition, PgTableConstraint, PgTableDefinition, PgTriggerDefinition } from "./types";
-export declare function serializeTable(table: PgTableDefinition<any, any> & {
-    constraints?: PgTableConstraint[];
-}): string;
-export declare function serializeIndexes(table: PgTableDefinition<any, any>, indexes?: PgIndexDefinition[]): string[];
-export declare function serializeTriggers(table: PgTableDefinition<any, any>, triggers?: PgTriggerDefinition[]): string[];
